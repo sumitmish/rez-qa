a7d7e8a991a33b5de403edc037252b5113e5d7d3
sumitmish <97815738+sumitmish@users.noreply.github.com>
2022-03-04 00:59:36 -0500

    added scrolling to bottom action in commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('scrollToBottom', ()=>{
+    cy.scrollTo('bottom')
+    cy.wait(1000)
+    cy.scrollTo('bottom')
+})
